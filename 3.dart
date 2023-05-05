5874c3aad93cb32ed917a2d2571bdaad0b37b31a
Abdelrahman Swidan <eng.abdelrahman.swidan@gmail.com>
2023-05-05 22:57:59 +0300

    problem 3

---
+++ 3.dart
@@ -0,0 +1,8 @@
+double formula(double p, double t, double r) {
+  return (p * t * r) / 100;
+}
+
+void main() {
+//Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
+  print(formula(5, 5, 5));
+}
