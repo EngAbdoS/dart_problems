73730816ac086c8bd157df3feeb6cbc74da2219a
Abdelrahman Swidan <eng.abdelrahman.swidan@gmail.com>
2023-05-05 23:00:44 +0300

    problem 10

---
+++ 10.dart
@@ -0,0 +1,15 @@
+double count(double spil ,double people)
+{
+
+return spil/people;
+
+
+}
+void main(){
+//. Suppose, you often go to restaurant with friends and you have to split amount of bill. 
+//Write a program to calculate split amount of bill. Formula= (total bill amount) /
+//number of people
+
+print(count(555,5));
+
+}
