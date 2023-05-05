533f99b0a3df36b3b73c968c77e668bbc5e142c6
Abdelrahman Swidan <eng.abdelrahman.swidan@gmail.com>
2023-05-05 23:01:04 +0300

    problem 11

---
+++ 11.dart
@@ -0,0 +1,17 @@
+void evenNumbers(int start,int end)
+{
+start%2==0?{}:start++;
+while(start<=end)
+{
+  print(start);
+  start+=2;
+}
+
+
+}
+void main()
+{//Write a program in Dart to print even numbers between intervals using function
+
+evenNumbers(2,11);
+
+}
