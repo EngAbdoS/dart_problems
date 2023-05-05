d53b2f5b66885c2420700ea52c350d3bd6414a5a
Abdelrahman Swidan <eng.abdelrahman.swidan@gmail.com>
2023-05-05 23:02:29 +0300

    problem 16

---
+++ 16.dart
@@ -0,0 +1,13 @@
+List<String>names=["abdelrahman","gomah","mohamed","swidan"];
+void printNames(List<String>namesList)
+{
+
+for(var item in namesList)print(item);
+
+}
+void main()
+{//Create a list of names and print all names using list
+
+printNames(names);
+
+}
