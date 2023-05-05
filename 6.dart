9d2e65ba2d0478ef3d80b771cddf163d37e356f2
Abdelrahman Swidan <eng.abdelrahman.swidan@gmail.com>
2023-05-05 22:59:27 +0300

    problem 6

---
+++ 6.dart
@@ -0,0 +1,14 @@
+void start(double n1,double n2)
+{
+print("the quotient of two numbers is : ${n1~/n2} \nthe remainder of two numbers is : ${n1%n2}\n");
+
+}
+
+
+void main()
+{//Write a program to find quotient and remainder of two integers.
+
+start(11,5);
+
+}
+
