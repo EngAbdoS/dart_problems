95ab1ba46a9257345a1c62af33cc191b560b6c59
Abdelrahman Swidan <eng.abdelrahman.swidan@gmail.com>
2023-05-05 23:00:25 +0300

    problem 9

---
+++ 9.dart
@@ -0,0 +1,18 @@
+int toInt(String text)
+{
+ 
+ return int.parse(text);
+
+
+
+}
+
+void main()
+{//Write a dart program to convert String to int.
+
+
+String text="1818";
+print(toInt(text));
+
+
+}
