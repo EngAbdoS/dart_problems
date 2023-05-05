2a1a6602992f03b7acba7fb6659fbc33681b03be
Abdelrahman Swidan <eng.abdelrahman.swidan@gmail.com>
2023-05-05 23:50:12 +0300

    problem 2

--- problems solution/2.dart
+++ problems solution/2.dart
@@ -1,8 +1,26 @@
+Map<String,String>personData={};
+void addData()
+{
+  personData["name"]="swidan";
+  personData["phone"]="01095786953";
 
-void main()
+}
+
+void whereLength()
 {
 
 
-print("ZzZzZzZzZzZzZzzZzzzzZZzZZZZ");
+  personData.forEach((key, value) {key.length==4?print(key):{};});
+
+  
+}
+
+void main()
+{
+  //Create a map with name, phone keys and store some values to it. Use where to find all 
+keys that have length 4
+addData();
+
+whereLength();
 
 }
