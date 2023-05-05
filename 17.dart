40c76e88bb48dd2ddfcf5081ea83b015eb5c377e
Abdelrahman Swidan <eng.abdelrahman.swidan@gmail.com>
2023-05-05 23:02:46 +0300

    problem 17

---
+++ 17.dart
@@ -0,0 +1,14 @@
+Set<String> fruits={"Apple","Banana","Oranges","Strawberries","Raspberries"};
+void printSet(Set<String> fruits)
+{
+for(var item in fruits)print(item);
+
+
+
+}
+void main()
+{
+//Create a set of fruits and print all fruits using loop
+
+  printSet(fruits);
+}
