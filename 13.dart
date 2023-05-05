7e86dc927d433d267f42f37d70f3a004a64497d7
Abdelrahman Swidan <eng.abdelrahman.swidan@gmail.com>
2023-05-05 23:01:33 +0300

    problem 13

---
+++ 13.dart
@@ -0,0 +1,15 @@
+const double pi = 3.1415926535897932;
+
+double circleErea(double r)
+{
+
+return r*r*pi;
+
+}
+void main()
+{
+//Write a program in Dart that find the area of a circle using function
+print(circleErea(5));
+
+
+}
